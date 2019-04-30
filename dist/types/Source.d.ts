8b72929e4542cb533259666f1854834d8bd0e1dc
Daniel Khan <daniel@khan.io>
2019-04-30 09:37:15 +0200

    prep for release

--- dist/types/Source.d.ts
+++
@@ -1,5 +0,0 @@
-import { Metric } from './Metric';
-export interface Source {
-    addMetric(metric: Metric): any;
-    fetch(): any;
-}
--- dist/types/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { Source } from './Source';
-export { Metric } from './Metric';
